1ee895041e70f1e38d99bbf7d905b4d4e129f7aa
BilyanaYordanova <BilyanaYordanova@users.noreply.github.com>
2020-07-15 18:20:19 +0300

    bibi changes

--- server/registerCompetition.php
+++ server/registerCompetition.php
@@ -8,13 +8,19 @@
         $scorePath = $_POST['scorePath'];
         $rankPath = $_POST['rankPath'];
         $command = 'node scriping.js -u "' . $url . '" -n "' . $namePath . '" -p "' . $scorePath . '" -r "' . $rankPath . '"';
-        
+        // try{
+        //     $output = shell_exec($command);
+        // }catch(T_STRING $e){
+        //     echo $e;
+        // }
         $output = shell_exec($command);
+        // echo $output;
         $manage = json_decode($output, true);
         $conn = connectToDatabase('competitionTracker');     
         $query = "INSERT INTO Parsers (name, namePath, scorePath, rankPath)
         VALUES ('$name', '$namePath', '$scorePath', '$rankPath')";
         $conn->exec($query);
+        echo $output;
         print("<pre>".print_r($manage, true)."</pre>");
     }
 ?>
