4376e734749e9261472ec34a0e4b47373258fb1b
vasko <vaset0_98@abv.bg>
2020-07-21 16:47:33 +0300

    my changes

--- server/getCompetitionStats.php
+++ server/getCompetitionStats.php
@@ -1,7 +1,7 @@
 <?php
-    $command = 'node scriping.js -u "' . $url . '" -n "' . $namePath . '" -p "' . $scorePath . '" -r "' . $rankPath . '"';
+    // $command = 'node scriping.js -u "' . $url . '" -n "' . $namePath . '" -p "' . $scorePath . '" -r "' . $rankPath . '"';
 
-    $output = shell_exec($command);
-    $manage = json_decode($output, true);
-    print("<pre>".print_r($manage, true)."</pre>");
+    // $output = shell_exec($command);
+    // $manage = json_decode($output, true);
+    // print("<pre>".print_r($manage, true)."</pre>");
 ?>
--- server/registerCompetition.php
+++ server/registerCompetition.php
@@ -2,32 +2,32 @@
     include '../helpers/db.php';
 
     if($_SERVER['REQUEST_METHOD'] === 'POST'){
-        $name = $_POST['name'];
-        $url = $_POST['url'];
-        $namePath = $_POST['namePath'];
-        $scorePath = $_POST['scorePath'];
-        $rankPath = $_POST['rankPath'];
-        $useExisting = false;
-        $parser;
-        $conn = connectToDatabase('competitionTracker');
+        // $name = $_POST['name'];
+        // $url = $_POST['url'];
+        // $namePath = $_POST['namePath'];
+        // $scorePath = $_POST['scorePath'];
+        // $rankPath = $_POST['rankPath'];
+        // $useExisting = false;
+        // $parser;
+        // $conn = connectToDatabase('competitionTracker');
 
-        if(array_key_exists("useExisting", $_POST)){
-            $useExisting = true;
-            $query = 'SELECT * FROM Parsers WHERE name="'.$_POST['selectedParser'].'";';
+        // if(array_key_exists("useExisting", $_POST)){
+        //     $useExisting = true;
+        //     $query = 'SELECT * FROM Parsers WHERE name="'.$_POST['selectedParser'].'";';
 
-            $q = $conn->query($query);
-            $results = $q->fetch();
+        //     $q = $conn->query($query);
+        //     $results = $q->fetch();
 
-            $namePath = $results['namePath'];
-            $scorePath = $results['scorePath'];
-            $rankPath = $results['rankPath'];
-        }
+        //     $namePath = $results['namePath'];
+        //     $scorePath = $results['scorePath'];
+        //     $rankPath = $results['rankPath'];
+        // }
 
-        if(!array_key_exists("useExisting", $_POST)){
-            $query = "INSERT INTO Parsers (name, namePath, scorePath, rankPath)
-            VALUES ('$name', '$namePath', '$scorePath', '$rankPath')";
-            $conn->exec($query);
-        }
+        // if(!array_key_exists("useExisting", $_POST)){
+        //     $query = "INSERT INTO Parsers (name, namePath, scorePath, rankPath)
+        //     VALUES ('$name', '$namePath', '$scorePath', '$rankPath')";
+        //     $conn->exec($query);
+        // }
 
         include './getCompetitionStats.php';
     }
