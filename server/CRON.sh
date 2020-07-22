66372fb8a8362686ef97e5e0dedc49e2219a198f
vasko <vaset0_98@abv.bg>
2020-07-22 08:52:24 +0300

    final version

--- server/CRON.sh
+++ server/CRON.sh
@@ -1,4 +1,4 @@
-!/usr/bin/bash
+#!/usr/bin/bash
 for i in `eval echo {0..$6}` #One tick here is 20 seconds, so put period for which you want to use it
 do
 php ../server/CRON.php "$1" "$2" "$3" "$4" "$5"
